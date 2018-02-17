1c02c9d7761dfdc8736e92c3d3efe5daaab7809d
angeline bilodeau <Angeline.bilodeau@gmail.com>
2018-02-17 16:31:06 +0800

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180217070516) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
-    t.text "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+@category = ["chinese", "italian", "japanese", "french", "belgian"]
+puts 'Creating 5 restaurants...'
+
+ 5.times do
+ restaurant = Restaurant.new(
+  name: Faker::Name.name,
+  address: Faker::Address.city,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  category: @category.sample
+  )
+ restaurant.save
+
+ end
+ puts 'Finished!'
+
+
